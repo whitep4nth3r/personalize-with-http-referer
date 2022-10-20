e9d72c8966f0316f12a9394e4c4d39a34378f38e
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-10-20 09:41:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Personalize your static site using the HTTP referer header
+
+`index.html` is static page.
+
+Using Netlify Edge Functions, we can intercept a request to this page, check the value of the HTTP referer header, and
+rewrite the response of the HTML before it's returned to the browser. No client-side JavaScript required.
+
+[View the Edge Function code](netlify/edge-functions/referer.js)
+
+## Testing in development
+
+Use the [Netlify CLI](https://ntl.fyi/3Til6sY) to run this site locally.
+
+In the root of the project, enter the following command in your terminal:
+
+```bash
+netlify dev
+```
+
+To test the referer header, make sure the referer detection is set to http://localhost:8888/ and click on the "click
+here" link.
+
+To remove the HTTP referer header and banner in development, navigate to http://localhost:8888/ without clicking on the
+link.
---
+++ netlify/edge-functions/referer.js
@@ -0,0 +1,33 @@
+// HTMLRewriter docs: https://developers.cloudflare.com/workers/runtime-apis/html-rewriter/
+import { HTMLRewriter } from "https://ghuc.cc/worker-tools/html-rewriter/index.ts";
+
+export default async (request, context) => {
+  // get HTTP referer header
+  const referer = request.headers.get("referer");
+
+  // get the next HTTP response in the chain
+  const response = await context.next();
+
+  // if no referer, return the response
+  if (referer === null) {
+    return response;
+  }
+
+  // check for the referer we care about
+  // this is a basic example and you may want to get a little
+  // more fancy with fuzzy checks and multiple referers
+  if (referer !== "http://localhost:8888/") {
+    return response;
+  }
+
+  // if we do have a referer match, rewrite the element
+  // in the response HTML with a friendly message
+  return new HTMLRewriter()
+    .on("p#referer", {
+      element(element) {
+        element.setInnerContent("Hello, from an Edge Function that detected a specific HTTP referer header!");
+        element.setAttribute("class", "referer");
+      },
+    })
+    .transform(response);
+};
